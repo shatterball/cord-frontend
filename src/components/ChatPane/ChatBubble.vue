1796cdb62cbf69ed2ae2a71abbe84745a0bb16a6
Rohan Biswas <brainhacker550@gmail.com>
2020-02-11 22:35:05 +0530

    redesigned chat bubbles

--- src/components/ChatPane/ChatBubble.vue
+++ src/components/ChatPane/ChatBubble.vue
@@ -1,10 +1,8 @@
 <template>
-  <transition name="fade" mode="out-in">
-    <div :class="[type ? 'send' : 'recv']" class="bubble shared">
-      <p class="message shared">{{ msg.content }}</p>
-      <p class="time shared">{{date}}</p>
-    </div>
-  </transition>
+  <div :class="[type ? 'send' : 'recv']" class="bubble shared">
+    <p class="shared message">{{ msg.content }}</p>
+    <p class="time shared">{{date}}</p>
+  </div>
 </template>
 
 <script>
@@ -27,46 +25,75 @@ export default {
 <style scoped>
 .shared {
   font-family: sans-serif;
-  color: #93a1a1;
   margin: 0;
   padding: 0;
 }
 .bubble {
   margin: 0.3rem 0;
+  position: relative;
   font-size: 0.8rem;
   max-width: 30rem;
-  border-radius: 1rem;
+  border-radius: 0.3rem;
+}
+.message {
+  color: #000;
+  padding: 0.5rem 0.6rem;
+  padding-bottom: 0;
+  word-break: break-word;
+}
+.time {
+  color: #444;
+  font-size: 0.6rem;
+  padding: 0.5rem 0.6rem;
+  padding-top: 0;
 }
 .send {
   align-self: flex-end;
   color: #333;
-  border: 0.1rem solid #0087ff;
+  background: #75c0eb;
   padding: 0;
-  border-bottom-right-radius: 0;
+  border-top-right-radius: 0;
   margin-right: 5rem;
+}
+.send:after {
+  content: "";
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 0;
+  height: 0;
+  border: 10px solid transparent;
+  border-left-color: #75c0eb;
+  border-right: 0;
+  border-top: 0;
+  margin-right: -10px;
 }
 .send .time {
   text-align: right;
 }
 .recv {
   align-self: flex-start;
-  border: 0.1rem solid #d33682;
-  border-bottom-left-radius: 0;
+  color: #222;
+  background: #ccc;
   margin-left: 5rem;
+  border-top-left-radius: 0;
+}
+.recv:after {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 0;
+  height: 0;
+  border: 10px solid transparent;
+  border-right-color: #ccc;
+  border-left: 0;
+  border-top: 0;
+  margin-top: 0px;
+  margin-left: -10px;
 }
 .recv .time {
   text-align: left;
-}
-.message {
-  color: #333;
-  padding: 0.5rem 0.6rem;
-  padding-bottom: 0;
-  word-break: break-word;
-}
-.time {
-  font-size: 0.6rem;
-  padding: 0.5rem 0.6rem;
-  padding-top: 0;
 }
 @media screen and (max-width: 700px) {
   .bubble {
