16a93b61f84deabb137bdf1d20fff60cc1729fd6
Rohan Biswas <brainhacker550@gmail.com>
2020-05-05 00:17:41 +0530

    fixed svg icons

--- src/components/ChatPane/InputPane.vue
+++ src/components/ChatPane/InputPane.vue
@@ -5,12 +5,10 @@
         <VEmoji @select="appendEmoji" class="shadow" />
       </div>
       <button class="button shared" @click="showEmojiPicker = !showEmojiPicker" id="emojiButton">
-        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg viewBox="0 0 24 24">
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="1.5"
-            d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            fill="currentColor"
+            d="M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M15.5,8A1.5,1.5 0 0,1 17,9.5A1.5,1.5 0 0,1 15.5,11A1.5,1.5 0 0,1 14,9.5A1.5,1.5 0 0,1 15.5,8M8.5,8A1.5,1.5 0 0,1 10,9.5A1.5,1.5 0 0,1 8.5,11A1.5,1.5 0 0,1 7,9.5A1.5,1.5 0 0,1 8.5,8M12,17.5C9.67,17.5 7.69,16.04 6.89,14H17.11C16.3,16.04 14.33,17.5 12,17.5Z"
           />
         </svg>
       </button>
@@ -23,13 +21,19 @@
       @keyup="typingEvent"
       ref="inputBar"
     />
-    <img
+    <button
       @click="sendMessage"
       :class="{ 'enable': this.text.length > 0}"
       class="button shared"
       id="sendButton"
-      src="../../assets/send.svg"
-    />
+    >
+      <svg viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M22.78,17.91C22.94,18.16 23,18.42 23,18.7C23,19.08 22.87,19.39 22.57,19.64C22.27,19.89 21.94,20 21.56,20H19.08L12.42,8H11.58L4.92,20H2.39C1.92,20 1.53,19.8 1.22,19.38C0.91,18.96 0.89,18.5 1.17,18L10.78,1.69C11.09,1.22 11.5,1 12,1C12.53,1 12.92,1.22 13.17,1.69L22.78,17.91M4.78,22.31L12,9.38L19.22,22.31L18.5,23L12,20.34L5.44,23L4.78,22.31Z"
+        />
+      </svg>
+    </button>
   </div>
 </template>
 <script>
@@ -150,8 +154,8 @@ export default {
   padding: 0;
   background: #ddd;
   border-radius: 0;
-  height: 2rem;
-  width: 2rem;
+  height: 1.8rem;
+  width: 1.8rem;
   cursor: pointer;
   margin: 0 1.5rem;
   color: #666;
@@ -162,12 +166,8 @@ export default {
 .button:hover {
   color: #444;
 }
-.button:active {
-  transform: scale(0.85);
-}
 #sendButton {
-  height: 1.8rem;
-  width: 1.8rem;
+  transform: rotateZ(90deg);
 }
 @media screen and (max-width: 700px) {
   .input_pane {
